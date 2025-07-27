c0398bb89dadfd1312cce8662e881efc98252f92
Vybekid <felobitto@gmail.com>
2025-07-27 23:45:51 +0300

    fef

--- endtheweek.py
+++ endtheweek.py
@@ -3,21 +3,17 @@ import turtle
 screen = turtle.Screen()
 screen.bgcolor("black")
 
-# Create a turtle
 pen = turtle.Turtle()
-pen.speed(0) # Set the drawing speed to the fastest
+pen.speed(0)
 pen.hideturtle()
 
-# Define a vibrant rainbow color palette
 colors = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082", "#9400D3"]
 
-# Drawing loop
-sides = 7 # The number of points on the star
+sides = 4 
 for i in range(150):
-    pen.pencolor(colors[i % sides]) # Cycle through the rainbow colors
-    pen.forward(i * 3)              # Move forward by an increasing amount
-    pen.left(360 / sides + 1)       # Turn to create the star's points
-    pen.width(i / 50 + 1)           # Gradually increase the line width
+    pen.pencolor(colors[i % sides]) 
+    pen.forward(i * 3)              
+    pen.left(360 / sides + 1)       
+    pen.width(i / 50 + 1)           
 
-# Keep the window open until it's closed
 turtle.done()
